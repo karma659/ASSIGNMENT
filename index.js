6ef3dedb761faf743172c360538a53c289c08b0a
Akash <karmakarakash659@gmail.com>
2023-06-19 16:44:26 +0530

    add

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const marketPlaceInventorySchema = require("./models/marketPlaceInventorySchema"
 const {verifyToken} = require("./middlewares/authentication");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
-const path =require("path");
-const morgan = require("morgan");
+const path=require("path");
 
 var app = express();
 app.use(cors());
@@ -18,17 +17,15 @@ dotenv.config();
 connectDb();
 var port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'development') {
-   app.use(morgan('dev'))
- }
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+const dirname = path.resolve()
+app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '/frontend/build')))
+   app.use(express.static(path.join(dirname, '/frontend/build')))
  
    app.get('*', (req, res) =>
-     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+     res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
    )
  } else {
    app.get('/', (req, res) => {
